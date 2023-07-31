705cdf0bc901fafc46b4b2d3e907891f1753e2d7
Deimer23 <116132047+Deimer23@users.noreply.github.com>
2023-07-31 09:13:51 -0500

    Version 10

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -22,7 +22,7 @@ const ContactList = (props)=>{
         if(listContact !== null){          
             dispatch(addStorage(listContact));
         }
-      });
+      },[dispatch]);
 
     useEffect(()=>{
         localStorage.setItem('contact', JSON.stringify(contacts));
